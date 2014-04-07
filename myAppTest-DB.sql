dbd350f528106b35cb6d69ae6a5fbfcf24687012
ajimatahari <ajimatahari@leonardusPC>
2014-04-08 00:00:23 +0700

    add sqlDB

--- myAppTest-DB.sql
+++ myAppTest-DB.sql
@@ -1,59 +1,71 @@
-CREATE DATABASE  IF NOT EXISTS `myAppDb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `myAppDb`;
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
---
--- Host: 127.0.0.1    Database: myAppDb
--- ------------------------------------------------------
--- Server version	5.5.35-0ubuntu0.13.10.2
+-- Valentina Studio --
+-- MySQL dump --
+-- ---------------------------------------------------------
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- ---------------------------------------------------------
 
---
--- Table structure for table `User`
---
 
-DROP TABLE IF EXISTS `User`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(80) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `realName` varchar(255) NOT NULL,
-  `contactPhone` varchar(255) NOT NULL,
-  `emailAddress` varchar(255) NOT NULL,
-  `groupParent` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_id` (`id`),
-  KEY `index_id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- CREATE DATABASE "myAppDb" -------------------------------
+CREATE DATABASE IF NOT EXISTS `myAppDb` CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `myAppDb`;
+-- ---------------------------------------------------------
 
---
--- Dumping data for table `User`
---
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- CREATE TABLE "User" -------------------------------------
+CREATE TABLE `User` ( 
+	`id` Int( 255 ) AUTO_INCREMENT NOT NULL, 
+	`userName` VarChar( 80 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+	`password` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+	`realName` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+	`contactPhone` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+	`emailAddress` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+	`groupParent` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	 PRIMARY KEY ( `id` )
+, 
+	CONSTRAINT `unique_id` UNIQUE( `id` ) )
+CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 1;
+-- ---------------------------------------------------------
+
+
+-- CREATE TABLE "task" -------------------------------------
+CREATE TABLE `task` ( 
+	`idUser` Int( 255 ) AUTO_INCREMENT NOT NULL,
+	 PRIMARY KEY ( `idUser` )
+ )
+CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 1;
+-- ---------------------------------------------------------
+
+
+-- Dump data of "User" -------------------------------------
+-- ---------------------------------------------------------
+
+
+-- Dump data of "task" -------------------------------------
+-- ---------------------------------------------------------
+
+
+-- CREATE INDEX "index_id" ---------------------------------
+CREATE INDEX `index_id` USING BTREE ON `User`( `id` );
+-- ---------------------------------------------------------
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- ---------------------------------------------------------
 
--- Dump completed on 2014-04-04 23:51:54
+
